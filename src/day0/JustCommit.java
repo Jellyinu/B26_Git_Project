7c710ca263f48926420ca0bb16c5ee3b643c49a4
Kristina J Lemus <Krispyjas100@gmail.com>
2022-03-26 13:21:01 -0700

    practiced on my local commit

--- src/day0/JustCommit.java
+++ src/day0/JustCommit.java
@@ -5,4 +5,7 @@ public class JustCommit {
         System.out.println("I love java");
         System.out.println("working.....");
     }
+    // extra line 1
+    // extra line 2
+    //extra line 3
 }
