f76c12a652a98f5e83b7fbb55be79b951da5df5c
Kristina J Lemus <Krispyjas100@gmail.com>
2022-03-26 13:14:30 -0700

    I am trying my local commit

---
+++ src/day0/JustCommit.java
@@ -0,0 +1,8 @@
+package day0;
+
+public class JustCommit {
+    public static void main(String[] args) {
+        System.out.println("I love java");
+        System.out.println("working.....");
+    }
+}
